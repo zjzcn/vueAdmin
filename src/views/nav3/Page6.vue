63e1b6802f33da953cff50e544e92d605e49dc56
jinzhong.zhang <jinzhong.zhang@ele.me>
2017-11-21 10:22:05 +0800

    editor

--- src/views/nav3/Page6.vue
+++ src/views/nav3/Page6.vue
@@ -1,4 +1,85 @@
 <template>
-	<section>page6...
-	</section>
+	<div id="wrapper">
+		<div id="json-editor" class="editor"></div>
+	</div>
 </template>
+
+<script>
+  import ace from 'brace'
+  import 'brace/mode/json'
+  import 'brace/theme/eclipse'
+
+  export default {
+    data() {
+      return {
+        text1: 'function a() {};'
+      }
+    },
+    methods: {
+
+    },
+    mounted() {
+      var editor = ace.edit('json-editor');
+      editor.getSession().setMode('ace/mode/json');
+      editor.setTheme('ace/theme/eclipse');
+      editor.setValue([
+          '{'
+          , ' "language": "JSON",'
+          , ' "foo": "bar",'
+          , ' "trailing": "comma",'
+          , '}'
+        ].join('\n')
+      );
+      editor.clearSelection();
+    }
+  }
+</script>
+
+<style>
+	#wrapper {
+		position: absolute;
+		top: 50px;
+		bottom: 0;
+		left: 0;
+		right: 0;
+	}
+
+	div.editor {
+		position: absolute;
+		width: 50%;
+		height: 50%;
+	}
+
+	#javascript-editor {
+		left: 0;
+		top: 0;
+	}
+
+	#coffee-editor {
+		left: 0;
+		bottom: 0;
+	}
+
+	#json-editor {
+		top: 0;
+		left: 50%;
+		bottom: 0;
+	}
+
+	#lua-editor {
+		left: 50%;
+		bottom: 0;
+	}
+
+	h1 {
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		margin: auto;
+		background-color: #272822;
+		color: antiquewhite;
+		border: 2px solid black;
+		text-align: center;
+	}
+</style>
